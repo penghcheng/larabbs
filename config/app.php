aebcd8f9b32ddd23ae0056644af036d44a0a90aa
derek <22865900737@qq.com>
2018-10-29 22:58:40 +0800

    Finish user auth

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        HieuLe\Active\ActiveServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
 
     ],
 
