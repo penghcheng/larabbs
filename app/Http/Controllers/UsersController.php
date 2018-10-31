32de68e42f80b2c3e4a573bea1e1e1374abe5fe2
derek <22865900737@qq.com>
2018-10-31 15:29:39 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show',compact('user'));
@@ -16,11 +22,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
+        $this->authorize('update', $user);
+
         $data=$request->all();
 
         if($request->avatar){
@@ -32,7 +41,6 @@ class UsersController extends Controller
         }
         $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
-
     }
 
 }
