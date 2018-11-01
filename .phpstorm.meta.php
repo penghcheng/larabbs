a2f860481f9382c69d2e460d94e9197228cf16c3
derek <22865900737@qq.com>
2018-11-01 17:40:12 +0800

    Auto translate slug

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-01 15:29:48.
+    * Generated on 2018-11-01 17:26:32.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.44 on 2018-11-01 15:29:45.
+ * Generated for Laravel 5.5.44 on 2018-11-01 17:26:30.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
