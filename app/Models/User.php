a39135289a1e548fbb145d64fd98fda26713ba60
derek <22865900737@qq.com>
2018-11-01 14:05:22 +0800

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
