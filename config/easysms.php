ec109db58f7cc96596a6062edb0ad1f41c20f1be
derek <22865900737@qq.com>
2018-11-07 18:08:00 +0800

    add easysms

---
+++ config/easysms.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    // HTTP 请求的超时时间（秒）
+    'timeout' => 5.0,
+
+    // 默认发送配置
+    'default' => [
+        // 网关调用策略，默认：顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'huyi',
+        ],
+    ],
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'huyi' => [
+            'api_id' => env('HUYIWUXIAN_APIID'),
+            'api_key' => env('HUYIWUXIAN_APIKEY'),
+        ],
+    ],
+];
