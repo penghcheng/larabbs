1b3d0608cd0b9dd164f33736980a6316c4ce629e
derek <22865900737@qq.com>
2018-11-12 17:51:54 +0800

    social authorizations

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','phone', 'introduction', 'avatar',
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+        'weixin_openid', 'weixin_unionid'
     ];
 
     /**
