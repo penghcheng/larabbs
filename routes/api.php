51e35392bed53db7d52bfe8fb37144c9e29471b3
derek <22865900737@qq.com>
2018-11-14 16:31:52 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -5,32 +5,38 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
     ], function($api) {
+
+        // 游客可以访问的接口
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
-
         // 用户注册
         $api->post('users', 'UsersController@store')->name('api.users.store');
-
         // 图片验证码
         $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
-
         // 第三方登录
         $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')->name('api.socials.authorizations.store');
-
         // 登录
         $api->post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
-
         // 刷新token
         $api->put('authorizations/current', 'AuthorizationsController@update')->name('api.authorizations.update');
-
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')->name('api.authorizations.destroy');
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')->name('api.user.show');
+
+        });
+
     });
 });
